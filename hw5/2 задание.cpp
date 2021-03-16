1291cedbfe067fe1d495ab581d631a7ecec60530
Roman20021 <71008221+Roman20021@users.noreply.github.com>
2021-03-16 10:07:44 +0300

    hw5

---
+++ hw5/2 задание.cpp
@@ -0,0 +1,109 @@
+﻿using namespace std;
+#include <iostream>
+
+class List {
+public:
+	void show(int*& arr, int& size) {
+		for (int i = 0; i < size; i++) {
+			cout << arr[i] << ' ';
+		}
+		cout << endl;
+
+	}
+
+	int Get(int id, int*& arr) {
+		return arr[id];
+	}
+	void remove(int*& arr, int& size, int number) {
+		int k = 0;
+		int v = 0;
+		int* newarr = new int[size];
+		for (int i = 0; i < size; i++) {
+			if (number == arr[i] and v == 0) {
+				v++;
+				i++;
+
+			}
+			newarr[k] = arr[i];
+			k++;
+		}
+		size--;
+		delete[] arr;
+		arr = newarr;
+
+	}
+	void fill(int* arr, int size) {
+		for (int i = 0; i < size; i++) {
+			arr[i] = rand() % 10;
+		}
+	}
+	void append(int number, int*& arr, int& size) {
+		bool flag = true;
+		for (int i = 0; i < size; i++) {
+			if (arr[i] == number) {
+				flag = false;
+			}
+		}
+		if (flag) {
+			int v = 0;
+			int* newarr = new int[size + 1];
+			for (int i = 0; i < size; i++) {
+				newarr[i] = arr[i];
+			}
+			if (number < arr[size - 1]) {
+				for (int i = 0; i <= size; i++) {
+					if (number < newarr[i] and v == 0) {
+						v++;
+						newarr[i] = number;
+						i++;
+
+					}
+					if (v == 1) {
+						newarr[i] = arr[i - 1];
+					}
+				}
+			}
+			if (number > arr[size - 1]) {
+				newarr[size] = number;
+			}
+			size++;
+			delete[] arr;
+			arr = newarr;
+
+		}
+	
+	}
+
+	void sort(int*& arr, int size) {
+		for (int i = 0; i < size - 1; i++) {
+			for (int j = 0; j < size - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int с = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = с;
+				}
+			}
+		}
+	}
+};
+
+int main()
+{
+	List list;
+	int size = 3;
+	int* pt = &size;
+	int* arr = new int[size];
+	list.fill(arr, size);
+	list.show(arr, size);
+	list.sort(arr, size);
+	list.show(arr, size);
+	list.append(4, arr, size);
+	list.show(arr, size);
+	list.append(99, arr, size);
+	list.show(arr, size);
+	list.remove(arr, size, 1);
+	list.show(arr, size);
+	list.remove(arr, size, 7);
+	list.show(arr, size);
+	delete[] arr;
+}
---
+++ hw5/3 задание.cpp
@@ -0,0 +1,93 @@
+﻿
+using namespace std;
+#include <iostream>
+
+void Union(int *&arr1, int *&arr2, int& size1, int& size2){
+    bool flag = true;
+    for (int i = 0; i < size2; i++) {
+        for (int b = 0; b < size1; b++) {
+            if (arr1[b] == arr2[i]) {
+                flag = false;
+            }
+            }
+        if (flag) {
+            arr1[size1] = arr2[i];
+            size1++;
+
+        }
+        flag = true;
+        }
+
+}
+
+void Crossing(int*& arr1, int*& arr2, int& size1, int& size2) {
+    int k = 0;
+    int* newarr1 = new int(k);
+    for (int i = 0; i < size2; i++) {
+        for (int b = 0; b < size1; b++) {
+            if (arr1[b] == arr2[i]) {
+                arr1[k] = arr2[i];
+        
+            }
+
+        }
+        
+    }
+
+ 
+
+    
+}
+void Difference(int*& arr1, int*& arr2, int& size1, int& size2) {
+    int k = 0;
+    int* newarr1 = new int(size1);
+    bool flag = true;
+    for (int i = 0; i < size1; i++) {
+        for (int b = 0; b < size1; b++) {
+            if (arr1[i] == arr2[b]) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            newarr1[k] = arr1[i];
+            k++;
+        }
+    
+    }
+    size1 = k;
+    arr1 = newarr1;
+
+}
+
+int main()
+{
+    int size1;
+    int size2;
+    cout << "the number of digits in the first array";
+    cin >> size1;
+    cout << "the number of digits in the  second array";
+    cin >> size2;
+    cout << "fill the first array";
+    int* arr1 = new int(size1);
+    int* arr2 = new int(size2);
+    for (int i = 0; i < size1; i++) {
+        cin >> arr1[i];
+    }
+    cout << "fill the second array";
+    for (int i = 0; i < size2; i++) {
+        cin >> arr2[i];
+    }
+
+ 
+    Union(arr1, arr2, size1, size2);
+ 
+ 
+    for (int i = 0; i < size1; i++) {
+        cout << arr1[i] << ' ';
+    }
+
+
+    delete arr1;
+    delete arr2;
+
+}
